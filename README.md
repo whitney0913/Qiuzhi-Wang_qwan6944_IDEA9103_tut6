d7d178226ca5b54303a66d877c234439e1edc720
whitney0913 <qwan6944@uni.sydney.edu.au>
2024-05-29 23:04:39 +1000

    final individual coding_iteration6
    add more interpretations

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Qiuzhi-Wang_qwan6944_IDEA9103_tut6
 
-This is my Individual Task
+Individual Task的详细信息
+1.与作品互动的说明
+
+
+
+2.动画处理的详细信息
+我选择Perlin 噪声
--- p5_project/sketch.js
+++ p5_project/sketch.js
@@ -7,14 +7,17 @@ function setup() {
   background(65, 90, 119);
 }
 
+ // Draw the background rectangle with a different color
 function draw() {
   background(27, 38, 59); 
   drawBackgroundRect(50, 50, width - 100, height - 100, color(13, 27, 42));
 
+  //create colour
   let newBlue = '#41EAD4';
   let newYellow  = '#FBFF12';
   let newGreen = '#007f5f'; 
   
+  // Draw a circle divided up and down
   function drawSplitCircle(x, y, diameter, fillColor1 = newBlue, fillColor2 = newYellow) {
     fill(fillColor1);
     arc(x, y, diameter, diameter, PI, 0);
@@ -24,6 +27,7 @@ function draw() {
     ellipse(x, y, diameter, diameter);
   }
 
+  // Draw a circle divided right and left
   function drawSplitCircleLR(x, y, diameter, fillColor1 = newBlue, fillColor2 = newYellow) {
     fill(fillColor1);
     arc(x, y, diameter, diameter, HALF_PI, HALF_PI + PI);
@@ -33,6 +37,7 @@ function draw() {
     ellipse(x, y, diameter, diameter);
   }
 
+   // Draw a circle divided by red and green
   function drawSplitCircleTopRed(x, y, diameter, fillColor1 = newYellow, fillColor2 = newBlue) {
     fill(fillColor1);
     arc(x, y, diameter, diameter, PI, 0);
@@ -42,50 +47,63 @@ function draw() {
     ellipse(x, y, diameter, diameter);
   }
 
+  // Central position
   let centerX = width / 2;
   let centerY = height / 2;
 
+  // List of diameters of circles
   let diameters = [80, 50, 30, 60];
   let noiseX = noise(t) * 100 - 50;
   let noiseY = noise(t + 1000) * 100 - 50;
 
   drawSplitCircle(centerX + noiseX, centerY + noiseY, diameters[0]);
 
+  // Left arm circle
   drawSplitCircle(centerX - 0.82 * diameters[0] + noiseX, centerY + noiseY, diameters[1]);
   drawSplitCircle(centerX - 0.7 * diameters[0] - diameters[1] + noiseX, centerY + noiseY, diameters[2]);
   drawSplitCircleLR(centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2 + noiseX, centerY - 2 * diameters[2] / 2 + noiseY, diameters[3]);
 
+  // Right arm circle
   drawSplitCircle(centerX + 0.88 * diameters[0] + noiseX, centerY + noiseY, diameters[3]);
   drawSplitCircle(centerX + 1.26 * diameters[0] + diameters[2] + noiseX, centerY + noiseY, diameters[3]);
 
   drawLine(centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2 + noiseX, centerY + noiseY, centerX + 1.26 * diameters[0] + diameters[2] + diameters[3] / 2 + noiseX, centerY + noiseY);
   drawLine(centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2 + noiseX, centerY - 2 * diameters[2] / 2 + diameters[3] / 2 + noiseY, centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2 + noiseX, centerY - 2 * diameters[2] / 2 - diameters[3] / 2 + noiseY);
 
+  // List of diameters of circles
   diameters = [60, 50, 80, 30];
 
+  // Draw the bottom rectangle here
   drawBottomRectangles(centerX, centerY + 1.11 * diameters[1] + diameters[2] + diameters[3], 300, 50, newGreen);
 
+  // Draw a small white rectangle over the blue rectangle
   fill(224, 225, 221);
   noStroke();
   rect(centerX - 105, centerY + 1.72 * diameters[1] + diameters[2] + diameters[3], 50, 60);
 
+  // Draw a blue circle on the small white rectangle
   fill(119, 141, 169);
   noStroke();
   ellipse(centerX - 80, centerY + 2.3 * diameters[1] + diameters[2] + diameters[3], 50);
 
+  // Draw a small white rectangle over the blue rectangle
   fill(224, 225, 221);
   rect(centerX + 55, centerY + 1.72 * diameters[1] + diameters[2] + diameters[3], 50, 60);
 
+  // Draw a small blue circle over the small white rectangle
   fill(119, 141, 169);
   ellipse(centerX + 80, centerY + 2.3 * diameters[1] + diameters[2] + diameters[3], 60, 60);
 
+  // Draw a small white rectangle
   fill(224, 225, 221);
   noStroke();
   rect(centerX + 15, centerY + 1.72 * diameters[1] + diameters[2] + diameters[3], 20, 60);
 
+  // Upper median radius
   drawSplitCircleLR(centerX + noiseX, centerY - 0.92 * diameters[0] + noiseY, diameters[3]);
   drawSplitCircleLR(centerX + noiseX, centerY - 1.68 * diameters[0] + noiseY, diameters[0]);
 
+  // Lower median radius
   drawSplitCircleLR(centerX + noiseX, centerY + 1.3 * diameters[1] + noiseY, diameters[1]);
   drawSplitCircleLR(centerX + noiseX, centerY + diameters[1] + diameters[2] + noiseY, diameters[2]);
   drawSplitCircleLR(centerX + noiseX, centerY + 1.71 * diameters[1] + diameters[2] + diameters[3] + noiseY, diameters[1]);
@@ -97,18 +115,22 @@ function draw() {
   drawSplitCircleTopRed(centerX - 1.6 * diameters[1] + noiseX, centerY + 1.71 * diameters[1] + diameters[2] + diameters[3] + noiseY, diameters[1]);
   drawLine(centerX - 1.6 * diameters[1] - diameters[1] / 2 + noiseX, centerY + 1.71 * diameters[1] + diameters[2] + noiseY, centerX + 1.7 * diameters[1] + diameters[0] / 2 + noiseX, centerY + 1.71 * diameters[1] + diameters[2] + noiseY);
 
+  // List of diameters of circles
   diameters = [30, 50, 80, 60];
 
+  // Right side vertical circle
   drawSplitCircleLR(centerX + 1.26 * diameters[2] + diameters[0] + diameters[3] / 2 + noiseX, centerY - 1.8 * diameters[0] + diameters[2] * 0.1 + noiseY, diameters[1]);
   drawSplitCircleLR(centerX + 1.26 * diameters[2] + diameters[0] + diameters[3] / 2 + noiseX, centerY - 1.48 * diameters[0] - diameters[1] + diameters[2] * 0.1 + noiseY, diameters[0]);
   drawSplitCircleLR(centerX + 1.26 * diameters[2] + diameters[0] + diameters[3] / 2 + noiseX, centerY - 0.65 * diameters[0] - diameters[1] - diameters[2] + diameters[2] * 0.1 + noiseY, diameters[2]);
   drawLine(centerX + 1.26 * diameters[2] + diameters[0] + diameters[3] / 2 + noiseX, centerY + noiseY, centerX + 1.26 * diameters[2] + diameters[0] + diameters[3] / 2 + noiseX, centerY - 0.65 * diameters[0] - diameters[1] - diameters[2] + diameters[2] * 0.1 - diameters[2] / 2 + noiseY);
 
+  // Draw background texture
   for (let i = 0; i < 30000; i++) {
     stroke(0, 187, 249, 50);
     point(random(width) + noise(t + i) * 10 - 5, random(height) + noise(t + i + 10000) * 10 - 5);
   }
 
+  // Draw and update circles from the circles array
   for (let circle of circles) {
     circle.x += noise(circle.noiseOffsetX) * 2 - 1;
     circle.y += noise(circle.noiseOffsetY) * 2 - 1;
@@ -121,11 +143,13 @@ function draw() {
   noiseOffset += 0.005;
 }
 
+// Draw the background rectangle
 function drawBackgroundRect(x, y, width, height, color) {
   fill(color);
   rect(x, y, width, height);
 }
 
+//Draw rectangles at the bottom of the canvas
 function drawBottomRectangles(centerX, y, rectWidth, rectHeight, rectColor) {
   let totalWidth = rectWidth * 3;
   let startX = centerX - totalWidth / 2;
@@ -146,7 +170,9 @@ function drawLine(x, y, x1, y1) {
   pop();
 }
 
+// Function to handle mouse press events
 function mousePressed() {
+  // Create a new circle at the mouse position with random properties
   let newCircle = {
     x: mouseX,
     y: mouseY,
@@ -156,9 +182,11 @@ function mousePressed() {
     color1: color(random(0, 3), random(4, 100), 228, 50),
     color2: color(random(0, 3), random(4, 100), 228, 50)
   };
+  // Add the new circle to the circles array
   circles.push(newCircle);
 }
-
+// Function to handle window resize events
 function windowResized() {
+    // Resize the canvas to the new window dimensions
   resizeCanvas(windowWidth, windowHeight);
 }
